c46b213495ec538c1cb871df8466cd5d6b8e71de
Apples4 <nhlanhla.mavuka@gmail.com>
2024-01-18 02:44:01 +0200

    corrected class inheritance

--- models/city.py
+++ models/city.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if models.storage_t == 'db':
         __tablename__ = 'cities'
--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.storage_t == 'db':
         __tablename__ = 'users'
