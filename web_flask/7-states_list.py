e007bcfc4b6859a4bcec075442bcf537749f49c3
Apples4 <nhlanhla.mavuka@gmail.com>
2024-02-19 17:18:21 +0200

    task 7 check again2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,7 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
-from models import *
+from models.state import State
 from models import storage
 app = Flask(__name__)
 
