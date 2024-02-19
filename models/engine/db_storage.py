52f7d33c1d2f2328d19c5f9bf62be0c631bd6fad
Apples4 <nhlanhla.mavuka@gmail.com>
2024-02-19 17:55:40 +0200

    task 7 check again

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,19 +21,12 @@ classes = {"Amenity": Amenity, "City": City,
 
 
 class DBStorage:
-    """manages storage of MySQL database"""
+    """interaacts with the MySQL database"""
     __engine = None
     __session = None
-    classes = {
-            "Amenity": Amenity,
-            "City": City,
-            "Place": Place,
-            "Review": Review,
-            "State": State,
-            "User": User}
 
     def __init__(self):
-        """Instantiate a DBStorage"""
+        """Instantiate a DBStorage object"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
@@ -47,14 +40,6 @@ class DBStorage:
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
-    def new(self, obj):
-        """ add the object to the current database session"""
-        self.__session.add(obj)
-
-    def save(self):
-        """commit all changes of the current database session"""
-        self.__session.commit()
-
     def all(self, cls=None):
         """query on the current database session"""
         new_dict = {}
@@ -66,6 +51,14 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
     def delete(self, obj=None):
         """delete from the current database session obj if not None"""
         if obj is not None:
@@ -74,36 +67,10 @@ class DBStorage:
     def reload(self):
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.
-                                    __engine, expire_on_commit=False)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """ returns objects based on class"""
-        if cls not in classes.values():
-            return None
-
-        all_cls = model.storage.all(cls)
-        for value in all_cls.value():
-            if (value.id == id):
-                return value
-
-        return None
-
-    def count(self, cls=None):
-        """ count number of objects in storage """
-        all_cls = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_cls:
-                coount += len(models.storage.all(clas).values())
-        else:
-            count = len(model.storage.all(cls).values())
-
-        return count
